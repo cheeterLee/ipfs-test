02f1a7839d2ac2f62afc7452d944ed2805050147
cheeter_Lee <johnson991210@gmail.com>
2023-01-13 14:10:28 +0800

    img rerender works !!!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent, FormEvent, useRef, useState } from "react"
 import { Web3Storage } from "web3.storage"
-import { Buffer } from 'buffer'
+import { Buffer } from "buffer"
 
 declare let process: {
 	env: {
@@ -12,6 +12,9 @@ const client = new Web3Storage({ token: process.env.VITE_API_TOKEN })
 
 function App() {
 	const [uploadImage, setUploadImage] = useState<any>()
+	const [displayImage, setDisplayImage] = useState<any>(
+		"https://via.placeholder.com/300/09f.png/fff"
+	)
 	const [imageFromIPFS, setImageFromIPFS] = useState<any>()
 
 	const captureFile = (e: ChangeEvent) => {
@@ -19,6 +22,14 @@ function App() {
 		const target = e.target as HTMLInputElement
 		if (!target.files) return
 		setUploadImage(target.files[0])
+	}
+
+	const retrieveImageFromIPFS = async (cid: string, imageName: string) => {
+		const res = await fetch(`https://${cid}.ipfs.dweb.link/${imageName}`)
+		const imageBlob = await res.blob()
+		const imageFromWeb3 = URL.createObjectURL(imageBlob)
+		console.log('imageFromWeb3', imageFromWeb3)
+		return imageFromWeb3
 	}
 
 	const retrieveImageFromWeb3Storage = async (cid: string) => {
@@ -37,18 +48,27 @@ function App() {
 	}
 
 	const uploadFile = async () => {
-		const rootCid = await client.put([uploadImage])
+		const rootCid = await client.put([uploadImage]) // Remember to use this as image url for rerender, the cid return from ipfs and rootCid does not match
 		const res = await client.get(rootCid)
 		if (res === null) return
-		const imageUrl = await retrieveImageFromWeb3Storage(rootCid)
-		console.log('imageUrl', imageUrl)
+		console.log("result", res)
+		const imageObjectBack = await res.files()
+		const { name } = imageObjectBack[0] 
+		console.log(name)
+		setDisplayImage(`https://${rootCid}.ipfs.w3s.link/${name}`)
+		// const imageDisplayUrl = await retrieveImageFromIPFS(cid, name)
+		// console.log('imageDisplayUrl', imageDisplayUrl)
+		// setDisplayImage(imageDisplayUrl)
+
+		// const imageUrl = await retrieveImageFromWeb3Storage(rootCid)
+		// console.log('imageUrl', imageUrl)
 	}
 
-	const handleSubmit = (e: FormEvent) => {
+	const handleSubmit = async (e: FormEvent) => {
 		e.preventDefault()
 		console.log("submitting....")
-		console.log("current state", uploadImage)
 		uploadFile()
+		// console.log("current state", uploadImage)
 	}
 
 	return (
@@ -63,7 +83,7 @@ function App() {
 				alignItems: "center",
 			}}
 		>
-			<img src="https://via.placeholder.com/300/09f.png/fff" alt="" />
+			<img src={displayImage} alt="image" />
 			<form onSubmit={handleSubmit}>
 				<input type="file" onChange={captureFile} />
 				<input type="submit" />
